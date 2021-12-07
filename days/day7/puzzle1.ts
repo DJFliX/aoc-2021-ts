36c6146f5d2ef9251eedef4d990e16737f7e91d8
Felix Mann <djflix@gmail.com>
2021-12-07 21:08:07 +0100

    Added day7

---
+++ days/day7/puzzle1.ts
@@ -0,0 +1,22 @@
+
+import { getPuzzleInput } from '../../lib/util';
+
+const getTotalFuelCost = (startPositions: number[], desiredEndPosition: number): number => {
+  return (startPositions.map(n => Math.abs(n - desiredEndPosition))).reduce((a, b) => a + b, 0)
+}
+
+getPuzzleInput(7).then((input) => {
+  const lines = input.split(",").map(Number)
+  let cheapestPosition = 0;
+  let cheapestPositionCost = Number.MAX_SAFE_INTEGER
+  for(let i = 0; i < 1000; i++) {
+    const fuelCostForStep = getTotalFuelCost(lines, i)
+    if(fuelCostForStep < cheapestPositionCost) {
+      cheapestPosition = i;
+      cheapestPositionCost = fuelCostForStep;
+      console.log(`Found a cheaper candidate: ${cheapestPosition} at ${cheapestPositionCost} fuel`)
+    }
+  }
+
+  console.log(`The cheapest position is ${cheapestPosition} at ${cheapestPositionCost} fuel`)
+})
