d425655e47de1a094487d555ae4d4702490caa3b
Felix Mann <djflix@gmail.com>
2021-12-07 21:40:24 +0100

    Reduce execution time for 7.2 to 2ms (from 180ms)

--- days/day7/puzzle2.ts
+++ days/day7/puzzle2.ts
@@ -1,27 +1,38 @@
 
 import { getPuzzleInput } from '../../lib/util';
 
-const getTotalFuelCost = (startPositions: number[], desiredEndPosition: number): number => {
-  return (startPositions.map(n => {
-    let distance = Math.abs(n - desiredEndPosition),
-      fuel = 0;
-    while(distance > 0) fuel += distance--
-    return fuel
-  })).reduce((a, b) => a + b, 0)
-}
+
 
 getPuzzleInput(7).then((input) => {
   const lines = input.split(",").map(Number)
+  const getTotalFuelCost = ( desiredEndPosition: number): number => {
+    return (lines.map(n => {
+      let distance = Math.abs(n - desiredEndPosition)
+      return (distance * (distance + 1)) /2
+    })).reduce((a, b) => a + b, 0)
+  }
+
   let cheapestPosition = 0;
   let cheapestPositionCost = Number.MAX_SAFE_INTEGER
-  for(let i = 0; i < 1000; i++) {
-    const fuelCostForStep = getTotalFuelCost(lines, i)
+  const start = new Date()
+
+  for(let i = 0; i < 1000; i+=30) {
+    const fuelCostForStep = getTotalFuelCost(i)
     if(fuelCostForStep < cheapestPositionCost) {
       cheapestPosition = i;
       cheapestPositionCost = fuelCostForStep;
-      console.log(`Found a cheaper candidate: ${cheapestPosition} at ${cheapestPositionCost} fuel`)
+    }
+  }
+  const cheapestPosFromIterationOne = Number(cheapestPosition);
+  for(let i = -15; i < 15; i++) {
+    const fuelCostForStep = getTotalFuelCost(cheapestPosFromIterationOne + i)
+    if(fuelCostForStep < cheapestPositionCost) {
+      cheapestPosition = cheapestPosFromIterationOne + i;
+      cheapestPositionCost = fuelCostForStep;
     }
   }
 
-  console.log(`The cheapest position is ${cheapestPosition} at ${cheapestPositionCost} fuel`)
+  const time = new Date().getTime() - start.getTime();
+
+  console.log(`The cheapest position is ${cheapestPosition} at ${cheapestPositionCost} fuel in ${time}ms`)
 })
