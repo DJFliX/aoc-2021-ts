bd95852956f84576f00b0807e0d222911d72191f
Felix Mann <djflix@gmail.com>
2021-12-11 10:22:00 +0100

    Finish day 10 puzzle 2

--- days/day10/puzzle2.ts
+++ days/day10/puzzle2.ts
@@ -2,17 +2,7 @@
 import { getPuzzleInput } from '../../lib/util';
 
 getPuzzleInput(10).then((input) => {
-  // const lines = input.split("\n").map(l => l.trim());
-  const lines = `[({(<(())[]>[[{[]{<()<>>
-    [(()[<>])]({[<{<<[]>>(
-    {([(<{}[<>[]}>{[]{[(<()>
-    (((({<>}<{<{<>}{[]{[]{}
-    [[<[([]))<([[{}[[()]]]
-    [{[{({}]{}}([{[{{{}}([]
-    {<[[]]>}<{[{[{[]{()[[[]
-    [<(<(<(<{}))><([]([]()
-    <{([([[(<>()){}]>(<<{{
-    <{([{{}}[<[[[<>{}]]]>[]]`.split("\n").map(l => l.trim());
+  const lines = input.split("\n").map(l => l.trim());
 
   const isOpeningCharacter = (character: string): boolean => {
     switch (character) {
@@ -41,13 +31,13 @@ getPuzzleInput(10).then((input) => {
 
   const getPointsForChar = (errorChar: string): number => {
     switch(errorChar) {
-      case ')':
+      case '(':
         return 1;
-      case ']':
+      case '[':
         return 2;
-      case '}':
+      case '{':
         return 3;
-      case '>':
+      case '<':
         return 4;
     }
     throw new Error("Invalid char provided to getPointsForChar")
@@ -55,6 +45,7 @@ getPuzzleInput(10).then((input) => {
 
   const followLineJourney = (line: string): number => {
     let lineOps: string[] = [];
+    let totalScore = 0;
     let characters = line.split("")
     for (let i = 0; i < characters.length; i++) {
       if(isOpeningCharacter(characters[i])) {
@@ -63,14 +54,17 @@ getPuzzleInput(10).then((input) => {
         if(wouldMatch(characters[i], lineOps[lineOps.length - 1])) {
           lineOps.pop();
         } else {
-          return 0 // Ignore this line: it gets no score. 
+          return 0 // Ignore this line: it gets no score because it is corrupt
         }
       }
     }
-    // Run autocomplete and return the score. 
-    return 0;
+    while(lineOps.length > 0) {
+      totalScore = (totalScore * 5) + getPointsForChar(lineOps.pop()!)
+    }
+    return totalScore;
   }
 
-  const autoCompletePoints = lines.map(followLineJourney).filter(points => points > 0)
-  console.log(autoCompletePoints)
+  const autoCompletePoints = lines.map(followLineJourney).filter(points => points > 0).sort((a, b) => a - b)
+  
+  console.log(autoCompletePoints[(autoCompletePoints.length - 1) / 2])
 })
